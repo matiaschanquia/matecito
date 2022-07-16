2de09615fb5da9d7690dbc68c31940e259b33723
matiaschanquia <matiaschanquianahuel@gmail.com>
2022-07-16 14:59:18 -0300

    mejoras en tipos

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+var inactivo;
+
+const botones = document.querySelectorAll(".boton-leer")
+const parrafos = document.querySelectorAll(".lineas-fuera")
+
+var contador = 0;
+botones.forEach((boton, index) => {
+    boton.addEventListener("click", function() {
+        cambiarLeer(index)
+    })
+})
+
+function cambiarLeer(index) {
+    if (contador !== 0)
+    {
+        botones[inactivo].classList.remove("display-none")
+        parrafos[inactivo].classList.add("lineas-fuera")
+    }
+    botones[index].classList.add("display-none")
+    parrafos[index].classList.remove("lineas-fuera")
+    inactivo = index
+    contador += 1
+}
